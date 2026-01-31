a3c3cdbf3b65482ca9c97584fc6812c2f689b192
Chico Bell <>
2026-01-31 14:15:02 +0000

    Refactored to use more deterministic Builders<[Entity]>.Filter.In

--- src/AspNetCore.IdentityServer.Mongo/Stores/ResourceStore.cs
+++ src/AspNetCore.IdentityServer.Mongo/Stores/ResourceStore.cs
@@ -46,18 +46,30 @@
         /// <returns></returns>
         public virtual async Task<IEnumerable<Duende.IdentityServer.Models.ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
         {
-            if (apiResourceNames is null)
-                throw new ArgumentNullException(nameof(apiResourceNames));
+            var names = apiResourceNames
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Distinct(StringComparer.Ordinal)
+                .ToArray();
 
-            var query = await Context.ApiResources.FindAsync(x => apiResourceNames.Contains(x.Name));
-            var results = await query.ToListAsync();
+            if (names.Length == 0)
+            {
+                Logger.LogDebug("Did not find {Apis} API resource in database", apiResourceNames);
+                return Array.Empty<Duende.IdentityServer.Models.ApiResource>();
+            }
 
-            var models = results.Select(x => x.ToModel());
+            var filter = Builders<Entities.ApiResource>.Filter.In(x => x.Name, names);
 
-            if (models.Any())
+            var results = await Context.ApiResources
+                .Find(filter)
+                .ToListAsync()
+                .ConfigureAwait(false);
+
+            var models = results.Select(x => x.ToModel()).ToArray();
+
+            if (models.Length > 0)
                 Logger.LogDebug("Found {Apis} API resource in database", results.Select(x => x.Name));
             else
-                Logger.LogDebug("Did not find {Apis} API resource in database", apiResourceNames);
+                Logger.LogDebug("Did not find {Apis} API resource in database", names);
 
             return models as IEnumerable<Duende.IdentityServer.Models.ApiResource>;
         }
@@ -69,14 +81,34 @@
         /// <returns></returns>
         public virtual async Task<IEnumerable<Duende.IdentityServer.Models.ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
         {
-            var names = scopeNames.ToArray();
+            ArgumentNullException.ThrowIfNull(scopeNames);
 
-            var query = await Context.ApiResources.FindAsync(x => x.Scopes.Any(s => names.Contains(s.Scope)));
-            var results = await query.ToListAsync();
+            var names = scopeNames
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Distinct(StringComparer.Ordinal)
+                .ToArray();
 
-            var models = results.Select(x => x.ToModel());
+            if (names.Length == 0)
+            {
+                Logger.LogDebug("Found {Apis} API resources in database", Array.Empty<string>());
+                return Array.Empty<Duende.IdentityServer.Models.ApiResource>();
+            }
 
-            Logger.LogDebug("Found {Apis} API resources in database", models.Select(x => x!.Name));
+            // ApiResource.Scopes is List<ApiResourceScope> and ApiResourceScope has property Scope (string)
+            var filter = Builders<Entities.ApiResource>.Filter.ElemMatch(
+                x => x.Scopes,
+                Builders<Entities.ApiResourceScope>.Filter.In(s => s.Scope, names)
+            );
+
+            var results = await Context.ApiResources
+                .Find(filter)
+                .ToListAsync()
+                .ConfigureAwait(false);
+
+            var models = results.Select(x => x.ToModel()).ToArray();
+
+            Logger.LogDebug("Found {Apis} API resources in database", models.Select(x => x?.Name));
+
 
             return models as IEnumerable<Duende.IdentityServer.Models.ApiResource>;
         }
@@ -86,12 +118,47 @@
         /// </summary>
         /// <param name="scopeNames"></param>
         /// <returns></returns>
-        public virtual async Task<IEnumerable<Duende.IdentityServer.Models.IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        public virtual async Task<IEnumerable<Duende.IdentityServer.Models.IdentityResource>> FindIdentityResourcesByScopeNameAsyncOld(IEnumerable<string> scopeNames)
         {
             var scopes = scopeNames.ToArray();
 
-            var query = await Context.IdentityResources.FindAsync(x => scopes.Contains(x.Name));
-            var results = await query.ToListAsync();
+            // var query = await Context.IdentityResources.FindAsync(x => scopes.Contains(x.Name));
+            // var results = await query.ToListAsync();
+
+            var filter = Builders<Entities.IdentityResource>
+                .Filter
+                .In(x => x.Name, scopes);
+
+            var results = await Context.IdentityResources
+                .Find(filter)
+                .ToListAsync();
+
+            Logger.LogDebug("Found {IdentityResources} identity scopes in database", results.Select(x => x.Name));
+
+            return results.Select(x => x.ToModel()).ToArray() as IEnumerable<Duende.IdentityServer.Models.IdentityResource>;
+        }
+
+        public virtual async Task<IEnumerable<Duende.IdentityServer.Models.IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            if (scopeNames is null) throw new ArgumentNullException(nameof(scopeNames));
+
+            var scopes = scopeNames
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Distinct(StringComparer.Ordinal)
+                .ToArray();
+
+            if (scopes.Length == 0)
+            {
+                Logger.LogDebug("Found {IdentityResources} identity scopes in database", Array.Empty<string>());
+                return Array.Empty<Duende.IdentityServer.Models.IdentityResource>();
+            }
+
+            var filter = Builders<Entities.IdentityResource>.Filter.In(x => x.Name, scopes);
+
+            var results = await Context.IdentityResources
+                .Find(filter)
+                .ToListAsync()
+                .ConfigureAwait(false);
 
             Logger.LogDebug("Found {IdentityResources} identity scopes in database", results.Select(x => x.Name));
 
@@ -105,12 +172,28 @@
         /// <returns></returns>
         public virtual async Task<IEnumerable<Duende.IdentityServer.Models.ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
         {
-            var scopes = scopeNames.ToArray();
+            if (scopeNames is null) throw new ArgumentNullException(nameof(scopeNames));
 
-            var query = await Context.ApiScopes.FindAsync(x => scopes.Contains(x.Name));
-            var results = await query.ToListAsync();
+            var scopes = scopeNames
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Distinct(StringComparer.Ordinal)
+                .ToArray();
+
+            if (scopes.Length == 0)
+            {
+                Logger.LogDebug("Found {Scopes} scopes in database", Array.Empty<string>());
+                return Array.Empty<Duende.IdentityServer.Models.ApiScope>();
+            }
+
+            var filter = Builders<Entities.ApiScope>.Filter.In(x => x.Name, scopes);
+
+            var results = await Context.ApiScopes
+                .Find(filter)
+                .ToListAsync()
+                .ConfigureAwait(false);
 
             Logger.LogDebug("Found {Scopes} scopes in database", results.Select(x => x.Name));
+
 
             return results.Select(x => x.ToModel()).ToArray() as IEnumerable<Duende.IdentityServer.Models.ApiScope>;
         }
